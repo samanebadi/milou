0bb422bf6018b176d5072889b0bc061f4d05fcea
samanebadi <saamanebadii11@gmail.com>
2025-08-12 18:22:38 +0330

    i added Util class to handle login exceptions.

--- src/main/java/milou/util/Util.java
+++ src/main/java/milou/util/Util.java
@@ -1,4 +1,24 @@
 package milou.util;
 
 public class Util {
-}
+        public static String emailInput(String input) {
+            if (input == null) return null;
+            input = input.trim();
+            if (!input.contains("@")) {
+                input = input + "@milou.com";
+            }
+            return input.toLowerCase();
+        }
+
+        public static String generatrCode() {
+            String chars = "jokapk2672jkdskakjdkllo";
+            StringBuilder sb = new StringBuilder(6);
+            java.util.Random rnd = new java.util.Random();
+            for (int i = 0; i < 6; i++) sb.append(chars.charAt(rnd.nextInt(chars.length())));
+            return sb.toString();
+        }
+
+        public static String truncate(String subject) {
+            if (subject == null) return "";
+            return subject.length() <= 255 ? subject : subject.substring(0, 255);  }
+    }
